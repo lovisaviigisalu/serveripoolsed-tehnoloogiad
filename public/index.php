e8307d115a39bd795347e99fcd9be397d9c3aad9
Leena Lovisa Viigisalu <lovisa.viigisalu@gmail.com>
2024-08-29 01:02:02 +0300

    Added and tested templating engine

--- public/index.php
+++ public/index.php
@@ -2,33 +2,31 @@
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
-
 use DI\Container;
 
 require __DIR__ . '/../vendor/autoload.php';
 
-
-
 $container = new Container();
+
+// Fix: Change the period to a comma here
+$container->set('templating', function(){
+    return new Mustache_Engine([
+        'loader' => new Mustache_Loader_FilesystemLoader(
+            __DIR__.'/../templates',
+            ['extension' => '']
+        )
+    ]);
+});
 
 AppFactory::setContainer($container);
 
 $app = AppFactory::create();
 
-$app->get('/', function (Request $request, Response $response,) {
-    $response->getBody()->write("Hello Slim!");
-    return $response;
-});
-
-$app->get('/hello/kadi', function (Request $request, Response $response,) {
-    $response->getBody()->write("Hello Kadi!");
-    return $response;
-});
-
-
-$app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
-    $name = ucfirst($args['name']);
-    $response->getBody()->write(sprintf("Hello, %s!", $name));
+$app->get('/hello/{name}', function(Request $request, Response $response, array $args = []){
+    $html = $this->get('templating')->render('hello.html', [
+        'name' => $args['name']
+    ]);
+    $response->getBody()->write($html);
     return $response;
 });
 
